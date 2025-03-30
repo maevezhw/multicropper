3ff3172508138d1e86076edeec3fd9f3f1a580ba
maevezhw <maeve.zahwa.adriana.crown.zaki@mail.ugm.ac.id>
2025-03-30 12:43:04 +0700

    fix again

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -3,12 +3,15 @@ set -eux  # Stop script jika terjadi error
 
 cd backend/app
 
+pip install --upgrade pip
 # Install Git LFS
 apt-get update && apt-get install -y git-lfs
 
 # Pull LFS files (download weights)
 git lfs install
 git lfs pull
+
+pip install -r requirements.txt
 
 # Jalankan server
 uvicorn main:app --host 0.0.0.0 --port 8000
