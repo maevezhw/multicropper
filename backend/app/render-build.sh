c2e8baa7e8480aa6a744fb9e011b26cc5438354a
maevezhw <maeve.zahwa.adriana.crown.zaki@mail.ugm.ac.id>
2025-03-30 12:23:15 +0700

    preparing backend for deployment

---
+++ backend/app/render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eux  # Stop script jika terjadi error
+
+# Install Git LFS
+apt-get update && apt-get install -y git-lfs
+
+# Pull LFS files (download weights)
+git lfs install
+git lfs pull
+
+# Jalankan server
+uvicorn main:app --host 0.0.0.0 --port 8000
