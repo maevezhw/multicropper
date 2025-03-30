811e257fcbd937741a69dc089a380af72ad331d0
maevezhw <maeve.zahwa.adriana.crown.zaki@mail.ugm.ac.id>
2025-03-30 12:30:06 +0700

    fix: make cropperSetting responsive

--- frontend/src/components/CropperSetting.jsx
+++ frontend/src/components/CropperSetting.jsx
@@ -195,27 +195,28 @@ const CropperSetting = ({ image, cropperRef, croppedImages, setCroppedImages, cr
             { activeTab === "manual-crop" ? (
                 <>
                     <h2 className="text-2xl font-semibold text-accentBlue mb-3">Resize Crop Box</h2>
-                    <div id="cropper-setting-size" className="grid grid-cols-2 gap-4 w-full">
-                        <div className="flex flex-row justify-between items-center">
-                            <label className="text-gray-600 text-lg font-normal mb-1 w-1/4">Width</label>
-                            <input
-                                placeholder="Width"
-                                className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-3/4" 
-                                value={localSize.width}
-                                onChange={handleWidthChange}
-                            />
-                        </div>
+                    <div id="cropper-setting-size" className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+    <div className="flex flex-row md:flex-row items-center w-full">
+        <label className="text-gray-600 text-lg font-normal mb-1 w-1/3 md:w-1/4">Width</label>
+        <input
+            placeholder="Width"
+            className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-2/3 md:w-3/4" 
+            value={localSize.width}
+            onChange={handleWidthChange}
+        />
+    </div>
 
-                        <div className="flex flex-row justify-between items-center">
-                            <label className="text-gray-600 text-lg font-normal mb-1 w-1/4">Height</label>
-                            <input 
-                                placeholder="Height"
-                                className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-3/4" 
-                                value={localSize.height}
-                                onChange={handleHeightChange}
-                            />
-                        </div>
-                    </div>
+    <div className="flex flex-row md:flex-row items-center w-full">
+        <label className="text-gray-600 text-lg font-normal mb-1 w-1/3 md:w-1/4">Height</label>
+        <input 
+            placeholder="Height"
+            className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-2/3 md:w-3/4" 
+            value={localSize.height}
+            onChange={handleHeightChange}
+        />
+    </div>
+</div>
+
 
                     <AspectRatioSelector setAspectRatio={setAspectRatio} />
                 </> ) : (
